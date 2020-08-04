c837f92b3f46238201f082244bbe36b19b2baadd
Gustavo Cavalcante <nowayhecodes@gmail.com>
2020-08-04 19:30:22 -0300

    find a way to integrate fetch with node

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,55 @@
 export class Toast {
-  static get() {}
-  static create() {}
-  static update() {}
-  static delete() {}
+  static async get(url: string, headers?: {}): Promise<JSON> {
+    return await fetch(url, {
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+        ...headers,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => data)
+      .catch((e) => e);
+  }
+
+  static async create(url: string, body: {}, headers: {}): Promise<JSON> {
+    return await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({ ...body }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+        ...headers,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => data)
+      .catch((e) => e);
+  }
+
+  static async update(url: string, body: {}, headers: {}): Promise<JSON> {
+    return await fetch(url, {
+      method: 'PATCH',
+      body: JSON.stringify({ ...body }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+        ...headers,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => data)
+      .catch((e) => e);
+  }
+
+  static async delete(url: string, body: {}, headers: {}): Promise<JSON> {
+    return await fetch(url, {
+      method: 'DELETE',
+      body: JSON.stringify({ ...body }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+        ...headers,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => data)
+      .catch((e) => e);
+  }
 }
