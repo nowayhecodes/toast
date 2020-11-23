ab88a8cae6345357bb7d7c7a1f2341b1d689196f
Gustavo Cavalcante <nowayhecodes@gmail.com>
2020-11-23 20:00:40 -0300

    Refactor test

--- src/__test__/toats.test.ts
+++ src/__test__/toats.test.ts
@@ -1,7 +1,18 @@
 import { Toast } from "../lib/index";
 
+jest.mock("../lib/index");
+
 describe("Toast", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   const toast = new Toast("https://jsonplaceholder.typicode.com");
+
+  test("Toast instance created", async () => {
+    expect(toast).toBeInstanceOf(Toast);
+  });
+
   test("Resolves '/posts/1' with the right object", async () => {
     const mockedResolvedValue = {
       userId: 1,
@@ -15,10 +26,12 @@ describe("Toast", () => {
         "nostrum rerum est autem sunt rem eveniet architecto",
     };
 
-    return toast.get("/posts/1").then((response) => {
-      expect(response).toStrictEqual(mockedResolvedValue);
-      expect(response).toBeInstanceOf(Object);
-    });
+    const spy = jest.spyOn(toast, "get");
+
+    (toast.get as jest.Mock).mockResolvedValueOnce(mockedResolvedValue);
+    expect(await toast.get("/posts/1")).toEqual(mockedResolvedValue);
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith("/posts/1");
   });
 
   test("Creates a new post", async () => {
@@ -28,9 +41,25 @@ describe("Toast", () => {
       body: "Too cool for school",
     };
 
-    return toast.create("/posts", mockedPostData).then((response) => {
-      expect(response).toBeInstanceOf(Object);
-    });
+    const mockedResolvedValue = {
+      userId: 1,
+      id: 101,
+      title: "Jest is cool",
+      body: "Too cool for school",
+      method: "POST",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    };
+
+    const spy = jest.spyOn(toast, "create");
+
+    (toast.create as jest.Mock).mockResolvedValueOnce(mockedResolvedValue);
+    expect(await toast.create("/posts", mockedPostData)).toEqual(
+      mockedResolvedValue
+    );
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith("/posts", mockedPostData);
   });
 
   test("Updates previously created post", async () => {
@@ -44,15 +73,22 @@ describe("Toast", () => {
       headers: { "Content-type": "application/json; charset=UTF-8" },
     };
 
-    return toast.update("/posts/101", mockedDataToUpdate).then((response) => {
-      expect(response).toBeInstanceOf(Object);
-      expect(response).toStrictEqual(mockedResponseData);
-    });
+    const spy = jest.spyOn(toast, "update");
+
+    (toast.update as jest.Mock).mockResolvedValueOnce(mockedResponseData);
+    expect(await toast.update("/posts/101", mockedDataToUpdate)).toEqual(
+      mockedResponseData
+    );
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith("/posts/101", mockedDataToUpdate);
   });
 
   test("Deletes a post", async () => {
-    return toast.delete("/posts/101").then((response) => {
-      expect(response).toBeInstanceOf(Object);
-    });
+    const spy = jest.spyOn(toast, "delete");
+
+    (toast.delete as jest.Mock).mockResolvedValueOnce(undefined);
+    expect(await toast.delete("/posts/101")).toEqual(undefined);
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith("/posts/101");
   });
 });
